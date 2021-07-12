dcde528984f259332f724d790de33b8294b0281d
Anne Machinda <annemachinda@gmail.com>
2021-07-12 16:41:56 +0100

    Added mandatory files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
+ */
+The third one
+int main(int argc, char *argv[])
+{
+int a;
+int b;
+int c;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+c = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", c);
+}
+return (0);
+}
