3f86af3285a8fdec4cdec2eb59d391576fe77083
Anne Machinda <annemachinda@gmail.com>
2021-07-14 15:54:02 +0100

    edited

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
-  
-int num_1, num_2, c;
-if (argc != 3)
-printf("Error\n");
-else
+int a, b, result;
+
+if (argc <= 2)
 {
-num_1 = atoi(argv[1]);
-num_2 = atoi(argv[2]);
-c = num_1 * num_2;
-printf("%d\n", c);
+printf("Error\n");
+return (1);
 }
+/* atoi convert a string to an int */
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+result = a * b;
+printf("%d\n", result);
 return (0);
 }
