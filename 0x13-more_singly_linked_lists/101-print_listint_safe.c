e171654ab8adeed60dfa384d765e326160dd5001
Anne Machinda <annemachinda@gmail.com>
2021-08-03 21:53:40 +0100

    Revert "time"
    This reverts commit eb1375ba772002141e60515a4222f18540220558.
    wwe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "lists.h"
+
 /**
- * print_listint_safe - prints a listint_t linked list
- * @head: pointer to head of list
- * Return: number of nodes printed
- */
+* print_listint_safe - a function that prints a listint_t
+*linked list
+* @head: points to the beginning of a linked list
+* Return: the number of nodes in the list
+**/
+
 size_t print_listint_safe(const listint_t *head)
 {
-	int diff;
+const listint_t *f_ptr, *s_ptr;
+size_t size;
 
-	register short count = 0;
+size = 0;
 
-	while (head)
-	{
-		diff = head - head->next;
-		count++;
-		printf("[%p] %i\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %i\n", (void *)head->next, head->next->n);
-			break;
-		}
+if (head == NULL)
+return (0);
 
-	}
-	return (count);
+s_ptr = head;
+f_ptr = head->next;
+
+while (f_ptr != NULL && f_ptr < s_ptr)
+{
+size += 1;
+printf("[%p] %i\n", (void *)s_ptr, s_ptr->n);
+s_ptr = s_ptr->next;
+f_ptr = f_ptr->next;
+}
+printf("[%p] %i\n", (void *)s_ptr, s_ptr->n);
+size += 1;
+if (f_ptr)
+printf("-> [%p] %i\n", (void *)f_ptr, f_ptr->n);
+
+return (size);
 }
