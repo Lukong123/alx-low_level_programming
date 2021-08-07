1aa23f96166687ee1a1fed2e4765167c468e33c3
Anne Machinda <annemachinda@gmail.com>
2021-08-07 11:19:33 +0100

    trying to adjust

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,15 +4,14 @@
  * @c: the character to be cheked
  * Return: 1 if letter, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
