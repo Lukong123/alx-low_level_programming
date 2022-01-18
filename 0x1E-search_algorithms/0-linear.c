7a8859cab1cbc4f3d36ba12c3c3b46e1940b0d9e
Anne Machinda <annemachinda@gmail.com>
2022-01-18 13:39:38 +0100

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,9 +1,9 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value by Linear search algorithm
+ * linear_search - searches for a value in an array of integer.
  * @array: a pointer to the first element of the array
- * @size: number of elements in array
+ * @size: the number of elements in array
  * @value: the value to search for
  * Return: index of the value, else -1
  */
